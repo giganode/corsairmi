c1f797f0fa61edaf4a6b3359e4a1aaa23ea47a7d
giganode <74789086+giganode@users.noreply.github.com>
2025-10-28 20:26:51 +0100

    Add another HX1500i

--- corsairmi.c
+++ corsairmi.c
@@ -76,8 +76,9 @@ static const uint16_t products[] = {
 	0x1c1e, /* HX1000i with USB-C */
 	0x1c08, /* HX1200i */
 	0x1c23, /* HX1200i with USB-C */
-	0x1c27,  /* HX1200i with USB-C */
+	0x1c27, /* HX1200i with USB-C */
 	0x1c1f, /* HX1500i */
+	0x1c02, /* HX1500i */
 };
 
 static void dump(const uint8_t *buf, size_t size)
